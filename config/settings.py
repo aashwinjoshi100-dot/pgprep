ec7bedc552d31a720eb6a2ac831a9253fd423f63
Aashwin Joshi <aashwinjoshi@Aashwins-MacBook-Air.local>
2026-06-07 16:38:23 +0545

    fix railway deployment config

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+allowed_host =[*]
 """
 Django settings for config project.
 
@@ -117,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MIDDLEWARE = [
+"whitenoise.middleware.WhiteNoiseMiddleware",
